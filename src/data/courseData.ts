ed732af8a5969a5c7e7bbb5f1e70cbf924a10b28
Micheal Owen <mowen8983@gmail.com>
2024-10-01 18:32:44 +0700

    chore: add course data to courseData.ts

---
+++ src/data/courseData.ts
@@ -0,0 +1,44 @@
+const courseData = [
+  {
+    id: 1,
+    imagesUrl: '/images/course section/html-5.svg',
+    altText: 'HTML 5',
+  },
+  {
+    id: 2,
+    imagesUrl: '/images/course section/css-3.svg',
+    altText: 'CSS 3',
+  },
+  {
+    id: 3,
+    imagesUrl: '/images/course section/javascript.svg',
+    altText: 'JavaScript',
+  },
+  {
+    id: 4,
+    imagesUrl: '/images/course section/react.svg',
+    altText: 'React',
+  },
+  {
+    id: 5,
+    imagesUrl: '/images/course section/git.svg',
+    altText: 'Git',
+  },
+  {
+    id: 6,
+    imagesUrl: '/images/course section/github.svg',
+    altText: 'Github',
+  },
+  {
+    id: 7,
+    imagesUrl: '/images/course section/vue.svg',
+    altText: 'Vue',
+  },
+  {
+    id: 8,
+    imagesUrl: '/images/course section/figma.svg',
+    altText: 'Figma',
+  },
+];
+
+export default courseData;
