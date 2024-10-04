289c852df7d2bd0969849103934c1095702f6b37
Micheal Owen <mowen8983@gmail.com>
2024-10-04 07:16:39 +0700

    chore: add features data

---
+++ src/data/featuresData.ts
@@ -0,0 +1,30 @@
+const featuresData = [
+  {
+    id: 1,
+    imagesUrl: '/public/images/features section/interactive.svg',
+    altText: 'Interactive Learning Icon',
+    title: 'Interactive Learning: Live coding sessions and hands-on exercises.',
+  },
+  {
+    id: 2,
+    imagesUrl: '/public/images/features section/mentorship.svg',
+    altText: 'Interactive Learning Icon',
+    title: 'Mentorship: Weekly 1-on-1 mentoring.',
+  },
+  {
+    id: 3,
+    imagesUrl: '/public/images/features section/community.svg',
+    altText: 'Interactive Learning Icon',
+    title:
+      'Community Support: Join a vibrant community of learners and alumni.',
+  },
+  {
+    id: 4,
+    imagesUrl: '/public/images/features section/certificate.svg',
+    altText: 'Interactive Learning Icon',
+    title:
+      'Certificate of Completion: Earn a certificate to showcase your skills.',
+  },
+];
+
+export default featuresData;
