ef166da0390cbd05d7de036e35c1a297f6351522
Micheal Owen <mowen8983@gmail.com>
2024-10-04 07:12:25 +0700

    feat: add Features component

---
+++ src/components/organisms/Features.tsx
@@ -0,0 +1,27 @@
+import featuresData from '../../data/featuresData';
+const Features = () => {
+  return (
+    <div className='mt-12 bg-slate-800 py-12'>
+      <h2 className='text-2xl text-headline text-center'>Course Features</h2>
+      {featuresData.map((features) => (
+        <ul
+          key={features.id}
+          className='flex items-start gap-3 px-4 text-paragraph'
+        >
+          <li className='flex mt-6 gap-3'>
+            <img
+              src={features.imagesUrl}
+              alt={features.altText}
+              width={24}
+              height={24}
+              className=''
+            />
+            <p>{features.title}</p>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default Features;
