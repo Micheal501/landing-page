52c6a09e7df9dab31ad79d0d04466cc8bee8b612
Micheal owen <mowen8983@gmail.com>
2024-10-07 08:02:29 +0700

    feat: change it to manual from using .map

--- src/components/organisms/Features.tsx
+++ src/components/organisms/Features.tsx
@@ -1,25 +1,67 @@
-import featuresData from '../../data/featuresData';
 const Features = () => {
   return (
-    <div className='mt-12 bg-slate-800 py-12'>
-      <h2 className='text-2xl text-headline text-center'>Course Features</h2>
-      {featuresData.map((features) => (
-        <ul
-          key={features.id}
-          className='flex items-start gap-3 px-4 md:ml-12 text-paragraph'
-        >
-          <li className='flex mt-6 gap-3'>
+    <div className='mt-12 bg-slate-800 py-12 lg:py-20'>
+      <h2 className='text-2xl text-headline text-center lg:text-3xl'>
+        Course Features
+      </h2>
+
+      <div className='lg:flex lg:justify-around'>
+        <ul className='flex flex-col gap-3 px-4 md:ml-12 text-paragraph'>
+          <li className='flex mt-6 gap-3 lg:items-center'>
             <img
-              src={features.imagesUrl}
-              alt={features.altText}
+              src='/public/images/features section/interactive.svg'
+              alt='Interactive Icon'
               width={24}
               height={24}
-              className=''
+              className='lg:w-16'
             />
-            <p>{features.title}</p>
+            <p className='lg:text-xl md:text-lg'>
+              Interactive Learning: Live coding sessions and hands-on exercises.
+            </p>
+          </li>
+          <li className='flex mt-6 gap-3 lg:items-center'>
+            <img
+              src='/public/images/features section/mentorship.svg'
+              alt='Mentorship Icon'
+              width={24}
+              height={24}
+              className='lg:w-16'
+            />
+            <p className='lg:text-xl md:text-lg'>
+              Mentorship: Weekly 1-on-1 mentoring.
+            </p>
           </li>
         </ul>
-      ))}
+
+        <ul className='flex flex-col gap-3 px-4 md:ml-12 text-paragraph'>
+          <li className='flex mt-6 gap-3 lg:items-center'>
+            <img
+              src='/public/images/features section/community.svg'
+              alt='Community Icon'
+              width={24}
+              height={24}
+              className='lg:w-16'
+            />
+            <p className='lg:text-xl md:text-lg'>
+              Community Support: Join a vibrant community of learners and
+              alumni.
+            </p>
+          </li>
+          <li className='flex mt-6 gap-3 lg:items-center'>
+            <img
+              src='/public/images/features section/certificate.svg'
+              alt='Certificate Features Icon'
+              width={24}
+              height={24}
+              className='lg:w-16'
+            />
+            <p className='lg:text-xl md:text-lg'>
+              Certificate of Completion: Earn a certificate to showcase your
+              skills.
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
