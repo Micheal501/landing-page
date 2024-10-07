56821757f0fed691fbcbb59fb31384b2deb00ef8
Micheal owen <mowen8983@gmail.com>
2024-10-07 08:01:07 +0700

    style: adjust style for larger screen

--- src/components/organisms/Curriculum.tsx
+++ src/components/organisms/Curriculum.tsx
@@ -9,12 +9,12 @@ const Curriculum = () => {
   };
 
   return (
-    <div className='bg-slate-800 py-12 mt-12'>
+    <div className='bg-slate-800 py-12 mt-12 lg:flex lg:flex-col lg:gap-3 lg:items-center'>
       <h2 className='text-headline text-2xl text-center mb-6'>
         What’s Inside the Course?
       </h2>
       {curriculumData.map((curriculum) => (
-        <div className='rounded-xl bg-slate-700 mx-8'>
+        <div className='rounded-xl bg-slate-700 mx-8 lg:w-2/6'>
           <div className='flex items-center justify-between mt-2 gap-2 p-3'>
             <h5 className='text-headline text-lg w-60'>{`Week ${curriculum.week}: ${curriculum.title}`}</h5>
 
